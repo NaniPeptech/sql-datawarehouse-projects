4084ef22fe50342d9d0cfa7e8c76b49a0a702753
cnani <naninka.coly@peptech-services.com>
2025-07-09 15:34:05 +0200

    previous version

--- create_bronze_ddl.sql
+++ create_bronze_ddl.sql
@@ -38,14 +38,13 @@ CREATE TABLE bronze.crm_prd_info (
 --Create the DDL for the file sales_details
 IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
 	DROP TABLE bronze.crm_sales_details;
-	
 CREATE TABLE bronze.crm_sales_details (
 	sls_ord_num  NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt NVARCHAR(50),
-	sls_ship_dt NVARCHAR(50),
-	sls_due_dt NVARCHAR(50),
+	sls_order_dt DATETIME,
+	sls_ship_dt DATETIME,
+	sls_due_dt DATETIME,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price FLOAT
